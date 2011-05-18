a1027b3b40156c476d5bc236849be502bebad063
Kevin Riggle <kevinr@free-dissociation.com>
2011-05-18 10:06:25 -0400

    log cloud image fetches

--- update-xplanet-background.sh
+++ update-xplanet-background.sh
@@ -2,12 +2,13 @@
 
 XPLANET_DESKTOP_FILE=$HOME/xplanet-desktop.png
 CLOUDS_FILE=$HOME/.xplanet/images/clouds_4096.jpg
+CLOUDS_LOG=$CLOUDS_FILE.log
 
 LATITUDE=30
 LONGITUDE=-71.090
 RADIUS=100
 GEOMETRY=1440x900
 
-$HOME/bin/download_clouds.py $CLOUDS_FILE
+$HOME/bin/download_clouds.py $CLOUDS_FILE 2>&1 | tee -a $CLOUDS_LOG
 xplanet -config bump -origin earth -latitude $LATITUDE -longitude $LONGITUDE -radius $RADIUS -geometry $GEOMETRY -num_times 1 -output $XPLANET_DESKTOP_FILE
 gconftool-2 --type string --set /desktop/gnome/background/picture_filename "$XPLANET_DESKTOP_FILE"
